a628136aa50175103fe9810cef05b175f6b8ff6e
dss <duanshuaishuai@pharmcube.com>
2024-11-19 09:00:19 +0800

    chore:foldText

--- packages/vue-coms/src/App.vue
+++ packages/vue-coms/src/App.vue
@@ -1,7 +1,14 @@
 <template>
-  <div class="c-w-200px">
+  <div>
+    <b>文案折叠组件</b>
+    <ul>
+      <li>1.行数可控</li>
+      <li>2.展开/收起操作区域可以自定义</li>
+    </ul>
+  </div>
+  <div class="c-w-260px c-border-1px">
     <text-fold
-      :line="1"
+      :line="2"
       :text="'TEST 测试组件TEST 测试组件TEST 测试组件TEST 测试组件TEST 测试组件TEST 测试组件TEST 测试组件组件'"
     ></text-fold>
   </div>
